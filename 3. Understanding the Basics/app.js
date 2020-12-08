f13e0c0c825832ed0afbb26a26b089800634f9af
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-08 21:51:51 +0800

    raw nodejs

--- 3. Understanding the Basics/app.js
+++ 3. Understanding the Basics/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log(`<url>: ${req.url}`);
@@ -7,7 +8,36 @@ const server = http.createServer((req, res) => {
     
     // process.exit();
 
-    
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body>
+            <form action="/message" method="POST">
+                <input type="text" name="message">
+                <button type="submit">Send</button>
+            </form>
+        </body>`);
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
