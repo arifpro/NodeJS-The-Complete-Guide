ef3a93c02d1db26ab95412a7520324991b6709a4
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-08 22:01:46 +0800

    separate the the routes into routes file

--- 3. Understanding the Basics/app.js
+++ 3. Understanding the Basics/app.js
@@ -1,50 +1,8 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    // console.log(`<url>: ${req.url}`);
-    // console.log(`\n<method>: ${req.method}`);
-    // console.log(req.headers);
-    
-    // process.exit();
+// custom
+const routes = require('./routes');
 
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body>
-            <form action="/message" method="POST">
-                <input type="text" name="message">
-                <button type="submit">Send</button>
-            </form>
-        </body>`);
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>This is title</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(1234);
