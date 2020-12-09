eb4d38edbad1570b797bd8030cd335f25231defd
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 02:07:10 +0800

    update

--- 4. Working with Express.js/app.js
+++ 4. Working with Express.js/app.js
@@ -9,7 +9,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found.</h1>');
+});
 
 app.listen(1234);
