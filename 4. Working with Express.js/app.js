14d37bb19db1444d8cea16793b78b658d7401965
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 01:56:29 +0800

    split routers

--- 4. Working with Express.js/app.js
+++ 4. Working with Express.js/app.js
@@ -3,23 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send(`<form action="/product" method="POST">
-        <input type="text" name="title">
-        <button type="submit">Add Product</button>
-    </form>`);
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello</h1>');
-});
-
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(1234);
