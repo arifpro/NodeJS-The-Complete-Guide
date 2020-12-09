22098e9f7c0cb94ce007f2720b80c6157a5aade8
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 00:44:52 +0800

    middleware

--- 4. Working with Express.js/app.js
+++ 4. Working with Express.js/app.js
@@ -6,6 +6,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();  // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello! from express!</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(1234);
