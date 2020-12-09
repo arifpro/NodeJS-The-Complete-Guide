15bc5ba612b4b4356d56fc9cbe3dc80cfe5823b8
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 01:40:31 +0800

    get data from form

--- 4. Working with Express.js/app.js
+++ 4. Working with Express.js/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/add-product', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<h1>Product</h1>');
+    res.send(`<form action="/product" method="POST">
+        <input type="text" name="title">
+        <button type="submit">Add Product</button>
+    </form>`);
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In the middleware');
     res.send('<h1>Hello</h1>');
 });
 
