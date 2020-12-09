a04c30db1b05e61063457f8ccf11cba5c715c419
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 00:56:57 +0800

    Update app.js

--- 4. Working with Express.js/app.js
+++ 4. Working with Express.js/app.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In the middleware');
-    next();  // Allows the request to continue to the next middleware in line
+    res.send('<h1>Product</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello! from express!</h1>');
+app.use('/', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>Hello</h1>');
 });
+
 
 app.listen(1234);
