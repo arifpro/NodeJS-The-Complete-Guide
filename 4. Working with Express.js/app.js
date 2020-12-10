ff13945d169d992c390a0cb67e278a2bc105eccc
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 12:24:20 +0800

    serving files statically

--- 4. Working with Express.js/app.js
+++ 4. Working with Express.js/app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path');
-
 const app = express();
 
 // routes
@@ -12,12 +10,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(1234);
