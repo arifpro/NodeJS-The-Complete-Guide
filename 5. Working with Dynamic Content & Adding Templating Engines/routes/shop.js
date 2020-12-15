ab2e6c664fa9e6043d63b2eebd96bbba4da16dd6
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-15 15:55:24 +0800

    dynamic content & template engine

---
+++ 5. Working with Dynamic Content & Adding Templating Engines/routes/shop.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
