00e0fd369884c4ca2117d228d6c91c6049aa9f48
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-15 21:21:28 +0800

    update and pug

--- 5. Working with Dynamic Content & Adding Templating Engines/routes/admin.js
+++ 5. Working with Dynamic Content & Adding Templating Engines/routes/admin.js
@@ -10,7 +10,8 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product'})
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
